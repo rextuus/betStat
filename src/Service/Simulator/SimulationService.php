96313f89de91fa6938e2851a11ff9745044206eb
rextuus <rextuus@gmail.com>
2022-02-26 12:29:36 +0100

    Added new simulation feature

--- src/Service/Simulator/SimulationService.php
+++ src/Service/Simulator/SimulationService.php
@@ -4,7 +4,9 @@
 namespace App\Service\Simulator;
 
 
+use App\Entity\Simulator;
 use App\Service\Simulator\Placement\PlacementService;
+use App\Service\Simulator\Simulator\SimulatorService;
 use App\Service\Simulator\SimulatorStrategy\SimulatorStrategyService;
 
 class SimulationService
@@ -20,20 +22,20 @@ class SimulationService
     private $simulatorStrategyService;
 
     /**
-     * @var SimulationService
+     * @var SimulatorService
      */
-    private $simulationService;
+    private $simulatorService;
 
     /**
      * SimulationService constructor.
      * @param PlacementService $placementService
      * @param SimulatorStrategyService $simulatorStrategyService
-     * @param SimulationService $simulationService
+     * @param SimulationService $simulatorService
      */
-    public function __construct(PlacementService $placementService, SimulatorStrategyService $simulatorStrategyService, SimulationService $simulationService)
+    public function __construct(PlacementService $placementService, SimulatorStrategyService $simulatorStrategyService, SimulationService $simulatorService)
     {
         $this->placementService = $placementService;
         $this->simulatorStrategyService = $simulatorStrategyService;
-        $this->simulationService = $simulationService;
+        $this->simulatorService = $simulatorService;
     }
 }
