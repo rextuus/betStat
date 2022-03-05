a0f6d0e06a9cba21e2a405076187ee3367a1ad1b
rextuus <rextuus@gmail.com>
2022-03-05 23:36:29 +0100

    Fixed bet collection mechanism

--- src/Service/Import/UpdateService.php
+++ src/Service/Import/UpdateService.php
@@ -506,6 +506,13 @@ class UpdateService
                 $this->fixtureService->updateFixture($fixtureToUpdate, $fixtureUpdate);
                 $this->logger->info(sprintf("Updated result for fixture with Id %d: %s", $fixtureToUpdate->getId(), $fixtureToUpdate));
             }
+            else{
+                $fixtureToUpdate = $this->fixtureService->findByApiKey($fixture->getFixtureApiId());
+                $fixtureToUpdate->setPlayed(false);
+                $fixtureToUpdate->setResultDecorationDate(new DateTime());
+                $this->fixtureService->updateFixture($fixtureToUpdate, $fixtureUpdate);
+                $this->logger->info(sprintf("!!!Updated for fixture with Id %d: %s failed", $fixtureToUpdate->getId(), $fixtureToUpdate));
+            }
         }
 
         return true;
