1a2ebda4fcf6d86536dc318524054b18aa2054a8
rextuus <rextuus@gmail.com>
2022-03-24 18:04:24 +0100

    Improved fixture storing

--- src/Command/InitFootballApiManager.php
+++ src/Command/InitFootballApiManager.php
@@ -129,7 +129,7 @@ class InitFootballApiManager extends Command
         if (true) {
 
 //            $famousLeagues = [22, 16, 18, 15, 1, 4, 13, 37, 36, 12, 2, 25, 20,23,46,47,5,6,40,70,28,67,21,74,45,38,66,55,56];
-            $famousLeagues = [21,74,45,38,66,55,56];
+            $famousLeagues = [56,3, 33, 34];
             foreach ($famousLeagues as $famousLeagueId){
                 $famousLeague = $this->leagueService->findById($famousLeagueId);
                 $seasons = $this->seasonService->findByLeague($famousLeague);
@@ -146,6 +146,8 @@ class InitFootballApiManager extends Command
 
         // step 4: store odds for fixtures
         if (false){
+//            [15, 16, 3, 4, 33, 34, 46, 47, 27, 28]
+//            [De1, De2, En1, En2, It1, It2, Sp1, Sp2, Fr1, Fr2]
             $famousLeagues = [22, 16, 18, 15, 1, 4, 13, 37, 36, 12, 2, 25, 20,23,46,47,5,6,40,70,28,67,21,74,45,38,66,55,56];
             foreach ($famousLeagues as $famousLeagueId){
                 $famousLeague = $this->leagueService->findById($famousLeagueId);
